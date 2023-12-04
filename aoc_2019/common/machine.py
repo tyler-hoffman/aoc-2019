387dcd86de016a457da5f09162bb1c17e47a7cbd
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2023-12-04 17:35:03 -0500

    Update machine

--- aoc_2019/common/machine.py
+++ aoc_2019/common/machine.py
@@ -1,24 +1,43 @@
 from dataclasses import dataclass
+from typing import Callable, Optional
 
 
 @dataclass
 class Machine:
     code: list[int]
+    get_input: Optional[Callable[[], int]] = None
+    send_output: Optional[Callable[[int], None]] = None
 
     def run(self) -> None:
-        c = self.code
+        code = self.code
         pos = 0
         running = True
 
         while running:
-            op_code = self.code[pos]
+            op_code = self.code[pos] % 100
+            c = (self.code[pos] // 100) % 10
+            b = self.code[pos] // 1000 % 10
+            a = self.code[pos] // 10000 % 10
+
+            param_a = code[pos + 1] if c == 0 else pos + 1
+            param_b = code[pos + 2] if b == 0 else pos + 2
+            param_c = code[pos + 3] if a == 0 else pos + 3
+
             match op_code:
                 case 1:
-                    c[c[pos + 3]] = c[c[pos + 1]] + c[c[pos + 2]]
+                    code[param_c] = code[param_a] + code[param_b]
                     pos += 4
                 case 2:
-                    c[c[pos + 3]] = c[c[pos + 1]] * c[c[pos + 2]]
+                    code[param_c] = code[param_a] * code[param_b]
                     pos += 4
+                case 3:
+                    assert self.get_input is not None
+                    code[param_a] = self.get_input()
+                    pos += 2
+                case 4:
+                    assert self.send_output is not None
+                    self.send_output(code[param_a])
+                    pos += 2
                 case 99:
                     running = False
                 case _:

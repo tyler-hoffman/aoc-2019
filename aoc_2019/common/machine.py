a3590a3d25f8495bdb9b42375e5be1e5dfbe5db0
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2023-12-04 17:52:48 -0500

    Add param method

--- aoc_2019/common/machine.py
+++ aoc_2019/common/machine.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Callable, Optional
 
 
@@ -7,38 +7,51 @@ class Machine:
     code: list[int]
     get_input: Optional[Callable[[], int]] = None
     send_output: Optional[Callable[[int], None]] = None
+    pos: int = field(default=0, init=False)
 
     def run(self) -> None:
         code = self.code
-        pos = 0
         running = True
 
         while running:
-            op_code = self.code[pos] % 100
-            c = (self.code[pos] // 100) % 10
-            b = self.code[pos] // 1000 % 10
-            a = self.code[pos] // 10000 % 10
+            op_code = self.code[self.pos] % 100
+            c = self.code[self.pos] // 100 % 10
+            b = self.code[self.pos] // 1000 % 10
+            a = self.code[self.pos] // 10000 % 10
 
-            param_a = code[pos + 1] if c == 0 else pos + 1
-            param_b = code[pos + 2] if b == 0 else pos + 2
-            param_c = code[pos + 3] if a == 0 else pos + 3
+            param_a = code[self.pos + 1] if c == 0 else self.pos + 1
+            param_b = code[self.pos + 2] if b == 0 else self.pos + 2
+            param_c = code[self.pos + 3] if a == 0 else self.pos + 3
 
             match op_code:
                 case 1:
                     code[param_c] = code[param_a] + code[param_b]
-                    pos += 4
+                    self.pos += 4
                 case 2:
                     code[param_c] = code[param_a] * code[param_b]
-                    pos += 4
+                    self.pos += 4
                 case 3:
                     assert self.get_input is not None
                     code[param_a] = self.get_input()
-                    pos += 2
+                    self.pos += 2
                 case 4:
                     assert self.send_output is not None
                     self.send_output(code[param_a])
-                    pos += 2
+                    self.pos += 2
                 case 99:
                     running = False
                 case _:
                     assert False
+
+    def param(self, offset: int) -> int:
+        code = self.code
+        pos = self.pos
+        mode = code[pos] // (10 ** (4 - offset)) % 10
+
+        match mode:
+            case 0:
+                return code[pos + offset]
+            case 1:
+                return pos + offset
+            case _:
+                assert False

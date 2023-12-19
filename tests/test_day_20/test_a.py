e0d8730a63e0417a9d3bd8019a6192beb3f5157a
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2023-12-18 21:19:07 -0500

    Update test data

--- tests/test_day_20/test_a.py
+++ tests/test_day_20/test_a.py
@@ -4,16 +4,16 @@ from aoc_2019.day_20.a import get_solution, solve
 from aoc_2019.day_20.from_prompt import (
     SAMPLE_INPUT_1,
     SAMPLE_INPUT_2,
-    SAMPLE_SOLUTION_1,
-    SAMPLE_SOLUTION_2,
+    SAMPLE_SOLUTION_1_A,
+    SAMPLE_SOLUTION_2_A,
 )
 
 
 @pytest.mark.parametrize(
     "input, expected",
     [
-        (SAMPLE_INPUT_1, SAMPLE_SOLUTION_1),
-        (SAMPLE_INPUT_2, SAMPLE_SOLUTION_2),
+        (SAMPLE_INPUT_1, SAMPLE_SOLUTION_1_A),
+        (SAMPLE_INPUT_2, SAMPLE_SOLUTION_2_A),
     ],
 )
 def test_solve(input: str, expected: int):
